c732573415331b5f7f84a87c9d50e65a7d33d071
JoaccoG <joaquingodoy2407@gmail.com>
2023-05-02 20:59:11 +0200

    Create hero details component to separate components responsabilities

---
+++ src/app/hero-details/hero-details.component.spec.ts
@@ -0,0 +1,22 @@
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { HeroDetailsComponent } from './hero-details.component';
+
+describe('HeroDetailsComponent', () => {
+  let component: HeroDetailsComponent;
+  let fixture: ComponentFixture<HeroDetailsComponent>;
+
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      declarations: [HeroDetailsComponent],
+    }).compileComponents();
+
+    fixture = TestBed.createComponent(HeroDetailsComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
